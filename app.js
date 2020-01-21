ede994663a8881817971103dfb9ba77c0e8de755
ZubairAshraf111 <zubair8767844@gmail.com>
2020-01-21 15:10:15 +0500

    initial

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const script = require("./routes/api/script");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", script);
